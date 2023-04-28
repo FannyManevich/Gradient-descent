776bbb5ce818fbf3b12898afa633b143a4797014
FannyManevich <89281719+FannyManevich@users.noreply.github.com>
2023-04-28 20:02:35 +0300

    update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 from decimal import *
 
@@ -9,7 +10,7 @@ def loadData(filename):
     rows, fs = data.shape
     D = data[:, :fs-1]
     Y = data[:, fs-1]
-    # print("Read " + str(rows) + " rows")
+    print("Read " + str(rows) + " rows")
     return D, Y
 
 # -- 2 --
@@ -42,28 +43,77 @@ def computeErrors(Data, Y, Hypothesis):
             Errors = np.insert(Errors, i, (predictValue(i, Hypothesis) - Y[i]))
     return Errors
 
-def main():
+# -- 5 --
+def computeCost(Data, Y, Hypothesis):
+    m = np.shape(Data)[0]
+    error = computeErrors(Data, Y, Hypothesis)
+    cost = 0
+    for i in range(m):
+        cost = cost + (error[i] ** 2)
+        cost = cost / (2 * m)
+    return cost
+
+# -- 6 --
+def computeGradient(Data, Errors):
+    m = np.shape(Data)[0]
+    n = np.shape(Data)[1]
+    grad_vector = []
+    vector = np.vectorize(np.int_)
+    for i in range(m):
+        for j in range(n):
+            sum = ((Data[i][j]) * Errors[i])
+        grad_vector.append(sum / m)
+    return vector(grad_vector)
+
+# -- 7 --
+def updateHypothsis(Hypothesis, alpha, Gradient):
+    return Hypothesis - alpha * Gradient
+
+# -- 8 --
+def gradientDescent(filename, alpha, max_iter, threshold):
+    # -- Initialize --
+    if 'alpha' not in locals():
+        alpha = 0.1
+    elif 'max_iter' not in locals():
+        max_iter = 1000
+    elif 'threshold' not in locals():
+        threshold = 0.001
+    cost_J = math.inf
+    iter = 1
+    Costs = np.empty(0)
+
     # -- 1 --
+    # -- Loading data from file --
     D, Y = loadData("smartphone.txt")
-    # x1 = D[:, 0]
-    # x2 = D[:, 1]
+    x1 = D[:, 0]
+    x2 = D[:, 1]
 
-    # plt.scatter(x1, Y)
-    # plt.xlabel('x  Original price')
-    # plt.ylabel('y  New price')
-    # plt.title('Original price VS New price')
-    # plt.show()
+    # -- Graph 1 --
+    plt.scatter(x1, Y)
+    plt.xlabel('x  Original price')
+    plt.ylabel('y  New price')
+    plt.title('Original price VS New price')
+    plt.show()
 
-    # plt.scatter(x2, Y)
-    # plt.xlabel('x Age')
-    # plt.ylabel('y New price')
-    # plt.title('Age VS New price')
-    # plt.show()
+    # -- Graph 2 --
+    plt.scatter(x2, Y)
+    plt.xlabel('x Age')
+    plt.ylabel('y New price')
+    plt.title('Age VS New price')
+    plt.show()
 
-    # # -- 2 --
+    # -- 2 --
     # -- Adding a left unity column --
     vector = np.vectorize(np.int_)
-    D1 = vector(addOnesColumn(selectSingle(D, 0)))
+    D = vector(addOnesColumn(selectSingle(D, 0)))
+
+    Hypothesis = [0.0, 0.0]
+
+    do{
+        
+    }while(iter < max_iter):
+def main():
+
 
     # # -- 3 --
     # rowD1 = D1[0, :]
@@ -76,6 +126,7 @@ def main():
     # -- Creating hypothesis vector of zeros
     Hypothesis = np.zeros(Data.shape[0])
     print((computeErrors(Data, Y, Hypothesis)[:, np.newaxis]))
+
 
 
 if __name__ == '__main__':
