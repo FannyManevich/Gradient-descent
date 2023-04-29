f795f9e39c007256613efbfd36ed05065d4be82a
FannyManevich <89281719+FannyManevich@users.noreply.github.com>
2023-04-29 13:41:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# Fany Manevich 206116725
+# Ilona Grand 316179548
+
 import numpy as np
 import math
 import matplotlib.pyplot as plt
@@ -13,6 +16,7 @@ def loadData(filename):
     print("Read " + str(rows) + " rows")
     return D, Y
 
+
 # -- 2 --
 def selectSingle(D, n):
     #-- Selecting a single column --
@@ -20,11 +24,11 @@ def selectSingle(D, n):
 
 
 def addOnesColumn(D):
-    new_d = D.reshape(-1, 1)
+    # new_d = D.reshape(-1, 1)
 
     #-- add a column of 1's --
-    ones_column = np.ones((new_d.shape[0], 1), dtype=int)
-    return np.hstack((ones_column, new_d))
+    ones_column = np.ones((D.shape[0], 1), dtype=int)
+    return np.hstack((ones_column, D))
 
 
 # -- 3 --
@@ -32,16 +36,20 @@ def predictValue(rowD1, hypothesis):
     # -- Multiplication of two Matrices --
     return np.dot(rowD1, hypothesis)
 
+
 # -- 4 --
 def computeErrors(Data, Y, Hypothesis):
-    if Data.shape[0] == Y.shape[0] :
-        # -- Number of examples for study --
-        m = Data.shape[0]
+
+    # -- Number of examples for study --
+    m = Data.shape[0]
+    errors = np.array([])
+    
+    if Data.shape[0] == Y.shape[0]:
         # -- Vector of the prediction errors of the hypothesis --
-        Errors = np.zeros(m)
         for i in range(m):
-            Errors = np.insert(Errors, i, (predictValue(i, Hypothesis) - Y[i]))
-    return Errors
+            errors = np.insert(errors, i, (predictValue(Data[i, :], Hypothesis) - Y[i]))
+    return errors
+
 
 # -- 5 --
 def computeCost(Data, Y, Hypothesis):
@@ -50,27 +58,34 @@ def computeCost(Data, Y, Hypothesis):
     cost = 0
     for i in range(m):
         cost = cost + (error[i] ** 2)
-        cost = cost / (2 * m)
+    cost = cost / (2 * m)
     return cost
+
 
 # -- 6 --
 def computeGradient(Data, Errors):
     m = np.shape(Data)[0]
     n = np.shape(Data)[1]
     grad_vector = []
-    vector = np.vectorize(np.int_)
-    for i in range(m):
-        for j in range(n):
-            sum = ((Data[i][j]) * Errors[i])
+    vector = np.vectorize(np.float_)
+    for j in range(n):
+        sum = 0 
+        for i in range(m):
+            sum += ((Data[i][j]) * Errors[i])
         grad_vector.append(sum / m)
     return vector(grad_vector)
+
 
 # -- 7 --
 def updateHypothsis(Hypothesis, alpha, Gradient):
     return Hypothesis - alpha * Gradient
 
+
 # -- 8 --
 def gradientDescent(filename, alpha, max_iter, threshold):
+    cost_j = float('inf')
+    cost: list = []
+
     # -- Initialize --
     if 'alpha' not in locals():
         alpha = 0.1
@@ -78,9 +93,12 @@ def gradientDescent(filename, alpha, max_iter, threshold):
         max_iter = 1000
     elif 'threshold' not in locals():
         threshold = 0.001
-    cost_J = math.inf
-    iter = 1
-    Costs = np.empty(max_iter)
+
+    cost_j = math.inf
+    cost.append(cost_j)
+    iter = 0
+    costs = np.array([])
+    costs = np.append(costs, np.inf)
 
     # -- 1 --
     # -- Loading data from file --
@@ -105,30 +123,48 @@ def gradientDescent(filename, alpha, max_iter, threshold):
     # -- 2 --
     # -- Adding a left unity column --
     vector = np.vectorize(np.int_)
-    D = vector(addOnesColumn(selectSingle(D, 0)))
+    D = vector(addOnesColumn(D))
 
-    Hypothesis = np.zeros(D.shape[0])
+    Hypothesis = np.zeros(3)
 
-    while iter < max_iter &  Costs[iter] < threshold:
+    while iter < max_iter:
+        print(iter)
         # -- 4 --
-        Data = vector(addOnesColumn(selectSingle(D, 0)))
+        Data = D
         # -- Creating hypothesis vector of zeros
-        Errors = (computeErrors(Data, Y, Hypothesis)[:, np.newaxis])
+        # Errors = (computeErrors(Data, Y, Hypothesis)[:, np.newaxis])
+        Errors = (computeErrors(Data, Y, Hypothesis))
         # -- 5 --
-        Costs.append(computeCost(Data, Y, Hypothesis))
+        costs = np.append(costs, computeCost(Data, Y, Hypothesis))
         # -- 6 --
         Gradient = computeGradient(Data, Errors)
         # -- 7 --
         Hypothesis = updateHypothsis(Hypothesis, alpha, Gradient)
+        if abs(costs[iter-1] - costs[iter]) < threshold:
+            print(f"Breaking loop because improvement is under threshold. {costs[iter-1] - costs[iter]} < {threshold}")
+            break
         iter = iter + 1
 
-    print("Gradient descent terminating after {} iterations. Improvement was :  {} -below threshold  {}".format(iter, threshold -  Costs[iter], threshold))
+    print("Gradient descent terminating after {} iterations. Improvement was :  {} -below threshold  {}".format(iter, abs(costs[iter-1] - costs[iter]), threshold))
+    costs = np.delete(costs, [0])
+    # Hypothesis, cost, x = gradientDescent(
+    #     'smartphone.txt', 0.05, 2000, 0.0001)  # calculate gradient descent
 
-    return Hypothesis, Costs
+    x = np.arange(max_iter)
+    print(f"size of x: {np.shape(x)}")
+    print(f"size of x: {np.shape(costs)}")
+    plt.scatter(x, costs)
+    plt.xlabel("x - Iteration")
+    plt.ylabel("y - Cost")
+    plt.title('Iteration VS Cost')
+    plt.show()
+
+    return Hypothesis, costs
+
+
 def main():
-
     gradientDescent("smartphone", 0.1, 1000, 0.001)
+
 
 if __name__ == '__main__':
     main()
-
