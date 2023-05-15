5c14768320916e8f170435cbc7b8bec122fb54d5
FannyManevich <89281719+FannyManevich@users.noreply.github.com>
2023-05-15 18:39:04 +0300

    update

--- Logistic regression.py
+++ Logistic regression.py
@@ -1,21 +1,50 @@
-from main import loadData
 from main import addOnesColumn
+from main import selectSingle
+import numpy as np
+import math
+
 
 # Fany Manevich 206116725
 # Ilona Grand 316179548
 
-# -- 1 --
-# def sigmoid(z):
+# -- 1.1 --
+# -- calculating sigmoid g(z)
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
 
- # return newMatrix
 
-# -- 2 --
-# def predictValue(Example, Hypothesis):
-#
-#
+# -- 1.2 --
+def predictValue(Example, Hypothesis):
+    # -- Multiplication of two Matrices --
+    h = np.dot(Example, Hypothesis)
+
+    return sigmoid(h)
+
+
 # # -- 2.1 --
-# def computeCostAndGradient(D, Y, Hypothesis):
-#
+def computeCostAndGradient(D, Y, Hypothesis):
+    J = 0
+    m = Y.shape[0]
+    Gradient = np.zeros(D.shape[0])
+
+    for i in range(m):
+        # prediction value
+        h = predictValue(D[i], Hypothesis)
+
+        # calculating the price
+        J += (-Y[i] * math.log(h) - (1 - Y[i]) * math.log(1 - h))
+
+        # calculating the error i
+        error = h - Y[i]
+
+        # updating gradient matrix
+        for j in range(D[0].shape):
+            Gradient[j] += error * D[i][j]
+
+        J /= m
+        Gradient /= m
+
+    return J, Gradient
 # # -- 2.2 --
 #
 # # -- 4 --
@@ -28,17 +57,27 @@ from main import addOnesColumn
 def main():
     # -- 0 --
     # -- Loading data from file --
-    D, Y = loadData("ex2data1.txt")
+    data = np.genfromtxt('ex2data1.txt', dtype=float, delimiter=',')
+    fs = data.size
+    D = data[:, :fs - 1]
     x1 = D[:, 0]
     x2 = D[:, 1]
+    Y = D[:, 2]
 
     # -- Adding a left unity column --
-    vector = np.vectorize(np.int_)
-    D = vector(addOnesColumn(selectSingle(D, 1)))
+    vector = np.vectorize(np.float_)
+    D = vector(addOnesColumn(selectSingle(D, 2)))
 
+    # -- 1 --
+    # -- checking --
+    # matrix1 = np.array([[1, 2, 3], [4, 5, 6]])
+    # matrix2 = np.array([[-1, -2, -3], [-4, -5, -6]])
 
+    print(predictValue(Y, x1))
 
-
+    # -- 2.1 --
+    Hypothesis = [-10, 0.8, 0.08]
+    print(computeCostAndGradient(D, Y, Hypothesis))
 
 if __name__ == '__main__':
     main()
